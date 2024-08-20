9bbf8171432a2d3d64ca8806707f096757f9bcf0
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-08-20 14:22:56 -0300

    Refatorar

--- ChatOnline/ChatOnline/src/main/java/br/chat/ChatOnline/controller/chat/ChatController.java
+++ ChatOnline/ChatOnline/src/main/java/br/chat/ChatOnline/controller/chat/ChatController.java
@@ -1,6 +1,6 @@
-package br.chat.ChatOnline.controller;
+package br.chat.ChatOnline.controller.chat;
 
-import br.chat.ChatOnline.models.ChatMessage;
+import br.chat.ChatOnline.models.chat.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -10,7 +10,7 @@ public class ChatController {
 
     @MessageMapping("/sendMessage")
     @SendTo("/topic/messages")
-    public ChatMessage sendMessage(ChatMessage message) throws Exception {
+    public Message sendMessage(Message message) throws Exception {
         // Aqui você pode adicionar lógica adicional, se necessário
         return message; // A mensagem será enviada para todos os conectados
     }
